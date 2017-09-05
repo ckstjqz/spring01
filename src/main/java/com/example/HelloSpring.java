4e93f85dea594d56e5f5d05b4d51b23ceddac2f4
ckstjqz <ckstjqzz@nate.com>
2017-09-05 15:34:52 +0900

    first commit

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -1,5 +1,14 @@
 package com.example;
 
 public class HelloSpring {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Spring의 두가지 추가화면 설계");
+		System.out.println("1. DI(Dependency Injection)");
+		System.out.println("2. AOP(Aspect objected Programming");
+		
+	}
+	
 
 }
